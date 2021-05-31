8e172d5fd47d116392704652003ceb417ce26519
Viraj Shah <shahvirajd@gmail.com>
2021-05-31 19:45:13 -0400

    Setup fetching modlist.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import { DATAPATH, STATICPATH } from './macroexp.js';
+import * as path from 'path';
+import { URL } from 'url'; // in Browser, the URL in native accessible on window
+
+// ! jshint/ts checking is disable because of `import.meta.url`
+// jshint ignore:start
+// @ts-ignore
+const __filename = new URL('', import.meta.url).pathname;
+// Will contain trailing slash
+// @ts-ignore
+const __dirname = new URL('.', import.meta.url).pathname;
+//jshint ignore:end
 
 const app = express();
 
 app.use(express.static(STATICPATH));
 
 app.get('/', (req, res) => {
-    res.send([DATAPATH, STATICPATH]);
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, () => {
