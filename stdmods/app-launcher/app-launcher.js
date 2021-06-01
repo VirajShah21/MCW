683d9ce6996c4408df7c733a76085ecd237dce01
Viraj Shah <shahvirajd@gmail.com>
2021-06-01 09:44:07 -0400

    Refactored and improved the app launcher

--- stdmods/app-launcher/app-launcher.js
+++ stdmods/app-launcher/app-launcher.js
@@ -4,10 +4,40 @@ const AppRegistrar = {};
 class AppLauncher extends WindowPopup {
     constructor() {
         super({ width: 500, height: 500, resize: 'none' });
+        this.sideLength = 500;
     }
 
     static register(name, startCallback) {
         AppRegistrar[name] = startCallback;
+    }
+
+    redraw() {
+        const children = [];
+        for (const appName in AppRegistrar) {
+            children.push(
+                ContentBlock({
+                    styles: {
+                        padding: '12.5px',
+                    },
+                    children: [
+                        ClickButton({
+                            styles: {
+                                width: '100%',
+                            },
+                            text: appName,
+                            events: {
+                                click: () => {
+                                    AppRegistrar[appName]();
+                                },
+                            },
+                        }),
+                    ],
+                })
+            );
+        }
+        let curr = this.windowContainer.querySelector('.applist');
+        if (curr) curr.remove();
+        this.windowContainer.appendChild(ContentBlock({ children, classes: ['applist'] }));
     }
 
     show() {
@@ -20,32 +50,31 @@ class AppLauncher extends WindowPopup {
                 },
                 children: [
                     ClickButton({
-                        text: 'Hide',
+                        text: 'Toggle',
                         events: {
                             click: (ev) => {
-                                console.log(this);
-                                console.log(ev);
-                                this.resize(0, 0);
+                                if (this.sideLength == 500) {
+                                    this.resizeTo(75, 75);
+                                    this.sideLength = 75;
+                                } else {
+                                    this.resizeTo(500, 500);
+                                    this.sideLength = 500;
+                                    this.redraw();
+                                }
                             },
+                        },
+                        styles: {
+                            width: '50px',
+                            height: '50px',
+                            margin: '12.5px',
                         },
                     }),
                 ],
             })
         );
-        for (const appName in AppRegistrar) {
-            this.windowContainer.appendChild(
-                ClickButton({
-                    styles: {
-                        width: '100%',
-                    },
-                    text: appName,
-                    events: {
-                        click: () => {
-                            AppRegistrar[appName]();
-                        },
-                    },
-                })
-            );
-        }
+        this.redraw();
     }
 }
+
+const launcher = new AppLauncher();
+launcher.show();
