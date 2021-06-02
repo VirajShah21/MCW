a9f4293983b55ed8a1e151612c6635778074c4f9
Viraj Shah <shahvirajd@gmail.com>
2021-06-02 03:24:19 -0400

    Added jsdoc to build command

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+jsdoc src/**/*.js stdmods/**/*.js -d docs
+echo Done building jsdocs
 sass stdmods/windows/scss/main.scss stdmods/windows/windows.css
+echo Done compiling sass
 node src/index.js
+echo Terminated webserver
